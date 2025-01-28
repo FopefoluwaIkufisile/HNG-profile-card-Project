e2d48b731d381aeea0153f4caff6a8e9997490f0
Fopefoluwa Ikufisile <fope.ikufisile@gmail.com>
2025-01-28 16:57:02 -0600

    removed comments

--- index.js
+++ index.js
@@ -3,9 +3,5 @@ function updateTime() {
     const utcTime = now;
     document.getElementById("currentTimeUTC").textContent = utcTime;
 }
-
-// Update time immediately on load
 updateTime();
-
-// Optional: Update every second
 setInterval(updateTime, 1000);
